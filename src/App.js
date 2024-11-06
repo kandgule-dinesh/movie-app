115b58c18f008811afed4995fb15fd9b26300fe6
kandgule-dinesh <dineshkandagule@gmail.com>
2024-11-06 23:25:00 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,65 +1,9 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import './App.css';
-
-// function App() {
-//   const [movies, setMovies] = useState([]);
-
-//   const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
-//   const BASE_URL = 'https://api.themoviedb.org/3';
-
-//   const fetchMovies = async (category) => {
-//     try {
-//       let url = '';
-//       if (category === 'popular') {
-//         url = `${BASE_URL}/movie/popular?api_key=${API_KEY}`;
-//       } else {
-//         const genreId = category === 'action' ? 28 : 35;
-//         url = `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=${genreId}`;
-//       }
-//       const response = await axios.get(url);
-//       setMovies(response.data.results);
-//     } catch (error) {
-//       console.error('Error fetching movies:', error);
-//     }
-//   };
-
-//   return (
-//     <div className="App">
-//       <h1>Movie App</h1>
-//       <div className="sidebar">
-//         <button onClick={() => fetchMovies('popular')}>Popular</button>
-//         <button onClick={() => fetchMovies('action')}>Action</button>
-//         <button onClick={() => fetchMovies('comedy')}>Comedy</button>
-//       </div>
-//       <div className="movies">
-//         {movies.map((movie) => (
-//           <div key={movie.id} className="movie">
-//             <img
-//               src={
-//                 movie.poster_path
-//                   ? `https://image.tmdb.org/t/p/w200${movie.poster_path}`
-//                   : 'https://via.placeholder.com/200x300?text=No+Image'
-//               }
-//               alt={movie.title}
-//             />
-//             <h3>{movie.title}</h3>
-//             <p>Release Date: {movie.release_date}</p>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 // App.js
 import React, { useState } from 'react';
 import axios from 'axios';
-import Header from './components/header';
-import Footer from './components/footer';
-import Sidebar from './components/sidebar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
 import './App.css';
 
 function App() {
